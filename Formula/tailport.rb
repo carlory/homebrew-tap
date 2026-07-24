d61f10c4f191744f98f0b584a71adb5b07de378e
carlory <carlory@192.168.1.10>
2026-07-24 16:12:48 +0800

    Update Tailport to v0.1.1

--- Formula/tailport.rb
+++ Formula/tailport.rb
@@ -1,26 +1,26 @@
 class Tailport < Formula
   desc "Automatically expose loopback TCP services over Tailscale"
   homepage "https://github.com/carlory/tailport"
-  version "0.1.0"
+  version "0.1.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/carlory/tailport/releases/download/v0.1.0/tailport_v0.1.0_Darwin_arm64.tar.gz"
-      sha256 "f26620541aa0e832b27e40e10ca09595a83a3a3714546b92d61f3e2445e18a66"
+      url "https://github.com/carlory/tailport/releases/download/v0.1.1/tailport_v0.1.1_Darwin_arm64.tar.gz"
+      sha256 "5f25120362caf9d531b6ef418efcb02175f445c99b6770f3f4d99fa695633446"
     else
-      url "https://github.com/carlory/tailport/releases/download/v0.1.0/tailport_v0.1.0_Darwin_x86_64.tar.gz"
-      sha256 "6b45eeb50c315c6241a88b5c83b330a226f5d91499b5d36b08a5f6a4fd132013"
+      url "https://github.com/carlory/tailport/releases/download/v0.1.1/tailport_v0.1.1_Darwin_x86_64.tar.gz"
+      sha256 "bdb77f2de7e3cd23db089796b63593b37d704902d2e2ab410fe937a4f188bf36"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/carlory/tailport/releases/download/v0.1.0/tailport_v0.1.0_Linux_arm64.tar.gz"
-      sha256 "1ab1e762194649ea4d06a2d9bdf73e5c635cfa6cbc45fc332b6ebaf940f98b45"
+      url "https://github.com/carlory/tailport/releases/download/v0.1.1/tailport_v0.1.1_Linux_arm64.tar.gz"
+      sha256 "2d38dd3e9635ce593b0035513350b3e4f096eed5a2dea71c3a0034b928167680"
     else
-      url "https://github.com/carlory/tailport/releases/download/v0.1.0/tailport_v0.1.0_Linux_x86_64.tar.gz"
-      sha256 "6956c06ab745c8fac0db18c473360c6301ca624c72cd0407dd3ebb59bb989a01"
+      url "https://github.com/carlory/tailport/releases/download/v0.1.1/tailport_v0.1.1_Linux_x86_64.tar.gz"
+      sha256 "1cae3a1ae8d671812f877f6543a301db4c330a94b0adf76a5dfadfd9e86abdc3"
     end
   end
 
